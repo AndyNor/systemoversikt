aab0b7c64b62d7730dac9fd9a0ab5706e9267357
André Nordbø <andre.nordbo@gmail.com>
2019-10-29 22:43:20 +0100

    template tags

---
+++ systemoversikt/templatetags/templatetags.py
@@ -0,0 +1,35 @@
+from django import template
+from django.conf import settings
+from django.utils.html import format_html
+import string
+import random
+register = template.Library()
+
+''' https://stackoverflow.com/questions/14496978/fields-verbose-name-in-templates '''
+@register.simple_tag
+def get_verbose_field_name(instance, field_name):
+	return instance._meta.get_field(field_name).verbose_name
+
+@register.simple_tag
+def get_help_text(instance, field_name):
+	return instance._meta.get_field(field_name).help_text
+
+def id_generator(size=8, chars=string.ascii_uppercase):
+	return ''.join(random.choice(chars) for _ in range(size))
+
+@register.simple_tag()
+def explain_collapsed(text):
+	random_id = id_generator()
+	html = format_html('''
+		<a data-toggle="collapse" href="#{}" role="button" aria-expanded="false" aria-controls="collapseExample">
+		<img style="width: 10px; margin: 0px 4px;" src="{}open-iconic/svg/magnifying-glass.svg" alt="rediger"></a>
+		<div class="collapse" style="background-color: #ffffe2;" id="{}">{}</div>
+		''',
+		random_id,
+		settings.STATIC_URL,
+		random_id,
+		text,
+	)
+	return html
+
+
