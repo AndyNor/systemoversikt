c13a7112d8665a8209d20a8b6d1ceebd5ab49f64
André Nordbø <andre.nordbo@gmail.com>
2019-11-19 20:01:54 +0100

    URL-er skal ikke starte med /

--- systemoversikt/urls.py
+++ systemoversikt/urls.py
@@ -98,8 +98,8 @@ urlpatterns = [
 	#url(r'^systemurl/(?P<pk>\d{1,8})/$', views.systemurl, name='systemurl'),
 
 
-	url(r'^/script/bytt_kategori/(?P<fra>\d{1,8})/(?P<til>\d{1,8})/$', views.bytt_kategori, name='bytt_kategori'),
-	url(r'^/script/bytt_leverandor/(?P<fra>\d{1,8})/(?P<til>\d{1,8})/$', views.bytt_leverandor, name='bytt_leverandor'),
+	url(r'^script/bytt_kategori/(?P<fra>\d{1,8})/(?P<til>\d{1,8})/$', views.bytt_kategori, name='bytt_kategori'),
+	url(r'^script/bytt_leverandor/(?P<fra>\d{1,8})/(?P<til>\d{1,8})/$', views.bytt_leverandor, name='bytt_leverandor'),
 
 	#url(r'^tjenester/$', views.alle_tjenester, name='alle_tjenester'),
 	#url(r'^tjenester/(?P<pk>\d{1,8})/$', views.tjenestedetaljer, name='tjenestedetaljer'),
