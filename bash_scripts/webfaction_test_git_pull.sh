42a5de18573a9566bfe230b51a16250e910171c8
André Nordbø <andre.nordbo@gmail.com>
2019-12-01 18:11:17 +0100

    Update webfaction_test_git_pull.sh

--- bash_scripts/webfaction_test_git_pull.sh
+++ bash_scripts/webfaction_test_git_pull.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# execute permissions must be turned off on test server
+git config --local core.fileMode false
 # move to correct folder
 cd ~/webapps/kartoteket_test/myproject/systemoversikt/
 # update source code
@@ -7,5 +9,6 @@ git reset --hard origin/master
 # makemigrations (entironment is set up with python3.7)
 python3.7 manage.py makemigrations
 python3.7 manage.py migrate
+python3.7 manage.py collectstatic --noinput
 # restart apache
 ~/webapps/kartoteket_test/apache2/bin/restart
