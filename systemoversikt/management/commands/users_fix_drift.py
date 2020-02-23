6e7705860bfcc2f8fc23587139cb9c751ace6b0c
André Nordbø <andre.nordbo@gmail.com>
2020-02-23 13:22:29 +0100

    First version

---
+++ systemoversikt/management/commands/users_fix_drift.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Hensikten med denne koden er å fikse tilknytning virksomhet for DRIFT-brukere
+"""
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from systemoversikt.models import Virksomhet
+
+class Command(BaseCommand):
+	def handle(self, **options):
+
+		for u in User.objects.all():
+			if u.username[0:5].upper() == "DRIFT":
+				virksomhet_obj_ref = Virksomhet.objects.get(virksomhetsforkortelse="DRIFT")
+				u.profile.virksomhet = virksomhet_obj_ref
+				u.save()
+				print("Fikset %s" % u)
