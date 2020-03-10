e5fa404097ce933f6beddb0ba7ff6975abd567b1
André Nordbø <andre.nordbo@gmail.com>
2020-03-10 13:02:02 +0100

    Nytt script for å sette eier der det bare er én brukergruppe

---
+++ systemoversikt/management/commands/rydde_sys_uten_eier.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+Hensikten med denne koden er importere brukere i PRK for å kunne avdekke brukere som ikke burde være i AD.
+"""
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+import os
+import time
+import sys
+import json
+from systemoversikt.models import System, SystemBruk
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
+
+class Command(BaseCommand):
+	def handle(self, **options):
+		mangler = System.objects.filter(Q(driftsmodell_foreignkey=None) & ~Q(systemtyper=1) & Q(systemeier=None))
+		for m in mangler:
+			bruk = SystemBruk.objects.filter(system=m)
+			if len(bruk) == 1:
+				print("%s - %s" % (m, bruk[0].brukergruppe))
+				m.systemeier = bruk[0].brukergruppe
+				m.save()
