b09b6ab7490eba09f87110ff0edd305433bbe2dc
André Nordbø <andre.nordbo@gmail.com>
2020-04-14 11:47:42 +0200

    Ny utsending av e-post, rettet til kun systemer som ikke er deaktive

--- systemoversikt/management/commands/epost_eiere_og_forvaltere.py
+++ systemoversikt/management/commands/epost_eiere_og_forvaltere.py
@@ -16,32 +16,26 @@ class Command(BaseCommand):
 		"""
 		alle_ansvarlige = Ansvarlig.objects.all() #.filter(brukernavn__profile__virksomhet__in=[163, 145])
 		for ansvarlig in alle_ansvarlige:
-			systemer_eier_for = System.objects.filter(systemeier_kontaktpersoner_referanse=ansvarlig.pk)
-			systemer_forvalter_for = System.objects.filter(systemforvalter_kontaktpersoner_referanse=ansvarlig.pk)
+			systemer_eier_for = System.objects.filter(systemeier_kontaktpersoner_referanse=ansvarlig.pk).filter(~Q(ibruk=False))
+			systemer_forvalter_for = System.objects.filter(systemforvalter_kontaktpersoner_referanse=ansvarlig.pk).filter(~Q(ibruk=False))
 
 			if systemer_eier_for or systemer_forvalter_for: #hvis begge er tomme dropper vi å sende mail
 
 				systemer_eier = ""
 				for system in systemer_eier_for:
-					systemer_eier = "\n".join((systemer_eier, system.systemnavn))
+					systemer_eier = "\n * ".join((systemer_eier, system.systemnavn))
 
 				systemer_forvalter = ""
 				for system in systemer_forvalter_for:
-					systemer_forvalter = "\n".join((systemer_forvalter, system.systemnavn))
+					systemer_forvalter = "\n * ".join((systemer_forvalter, system.systemnavn))
 
 				url = reverse("ansvarlig",args=[ansvarlig.pk])
 				fornavn = ansvarlig.brukernavn.first_name
 
-				subject = "Oppdatering: invitasjon til forum for systemeiere i Oslo kommune"
+				subject = "Systemeierforum 16.04: Hvordan skaper man løsninger med god brukskvalitet?"
 				reply_to = "thomas.rigvar@byr.oslo.kommune.no"
 				recipients = [ansvarlig.brukernavn.email]
 				message = '''\
-Det har vært høy interesse for systemeierforum. Byrådsavdeling for finans har besluttet å endre lokale til Rådhusets nye auditorium, samt økt antall plasser.
-(Du finner lokalet ved å gå inn publikumsinngangen på Rådhuset på Fridtjof Nansens plass, og ned en etasje)
-Dato er fremdeles 12.12.2019 09:00-12:00.
-
-Om du forsøkte å melde deg på - men ikke fikk plass, kan du nå gjøre et nytt forsøk.
-https://tjenester.oslo.kommune.no/ekstern/arrangement/bestilling/bestilling.aspx?Arrangement=033ff634-de2b-4cc2-8778-3d74b76edce8
 
 ---
 
@@ -52,7 +46,7 @@ Du er registrert som eier av følgende systemer:
 Du er registrert som forvalter av følgende systemer:
 {systemer_forvalter}
 
-Gå til https://kartoteket.oslo.kommune.no{url} for en komplett oversikt over dine ansvarsområder knyttet til systemer og behandling.
+Gå til https://kartoteket.oslo.kommune.no{url} for en komplett oversikt over dine ansvarsområder.
 
 '''.format(fornavn=fornavn, systemer_eier=systemer_eier, systemer_forvalter=systemer_forvalter, url=url)
 
