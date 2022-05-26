6611cc77e17745dd2fbdb686a6e0141a012a6c05
André Nordbø <andre.nordbo@gmail.com>
2022-05-26 22:10:50 +0200

    Update admin.py

--- systemoversikt/admin.py
+++ systemoversikt/admin.py
@@ -1163,7 +1163,7 @@ class AutorisertBestillerAdmin(SimpleHistoryAdmin):
 @admin.register(CMDBdevice)
 class CMDBdeviceAdmin(admin.ModelAdmin):
 	actions = [export_as_csv_action("CSV Eksport")]
-	list_display = ('comp_name', 'device_type', 'model_id', 'sist_sett', 'last_loggedin_user', 'device_active', 'kilde_cmdb', 'kilde_prk', 'kilde_landesk', 'maskinadm_status', 'maskinadm_virksomhet_str', 'sub_name', 'comp_ip_address', 'comp_os_readable', 'comp_ram', 'dns', 'vlan')
+	list_display = ('comp_name', 'device_type', 'sist_oppdatert', 'model_id', 'sist_sett', 'last_loggedin_user', 'device_active', 'kilde_cmdb', 'kilde_prk', 'kilde_landesk', 'maskinadm_status', 'maskinadm_virksomhet_str', 'sub_name', 'comp_ip_address', 'comp_os_readable', 'comp_ram', 'dns', 'vlan')
 	search_fields = ('comp_name', 'sub_name__navn', 'comments', 'description')
 	list_filter = ('device_active', 'device_type', 'comp_os_readable', 'sist_sett', 'kilde_cmdb', 'kilde_prk', 'kilde_landesk', 'maskinadm_status', 'maskinadm_klienttype', 'landesk_manufacturer', 'landesk_os_release', 'landesk_os', 'maskinadm_virksomhet',)
 	autocomplete_fields = ('last_loggedin_user', 'sub_name', 'maskinadm_virksomhet', 'landesk_login')
