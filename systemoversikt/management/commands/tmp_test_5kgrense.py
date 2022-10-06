e886ea53b28a235b8dffe97ed80ad30411cb003d
André Nordbø <andre.nordbo@gmail.com>
2022-10-06 17:50:14 +0200

    Create tmp_test_5kgrense.py

---
+++ systemoversikt/management/commands/tmp_test_5kgrense.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Hensikten med denne koden er å fikse tilknytning virksomhet for DRIFT-brukere
+"""
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from systemoversikt.models import *
+
+class Command(BaseCommand):
+	def handle(self, **options):
+
+		l = ldap.initialize(os.environ["KARTOTEKET_LDAPSERVER"])
+		l.bind_s(os.environ["KARTOTEKET_LDAPUSER"], os.environ["KARTOTEKET_LDAPPASSWORD"])
+
+		ldap_path = "DC=oslofelles,DC=oslo,DC=kommune,DC=no"
+		ldap_filter = ('(&(objectCategory=Group)(cn=DS-SYE_APP_VIRK_GERICA))')
+		ldap_properties = ['cn', 'displayName', 'description']
+
+		query_result = l.search_s(
+				ldap_path,
+				ldap.SCOPE_BASE,
+				ldap_filter,
+				ldap_properties
+			)
+		print(query_result)
+		#for key in query_result:
+		#	virksomheter.append(key[1]["ou"][0].decode())
+		l.unbind_s()
+
