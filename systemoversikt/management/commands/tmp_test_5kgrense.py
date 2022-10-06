e519e55915ffdb7db2751dfacf9587f77fb5a7a7
André Nordbø <andre.nordbo@gmail.com>
2022-10-06 19:33:50 +0200

    Update tmp_test_5kgrense.py

--- systemoversikt/management/commands/tmp_test_5kgrense.py
+++ systemoversikt/management/commands/tmp_test_5kgrense.py
@@ -12,7 +12,10 @@ import os
 class Command(BaseCommand):
 	def handle(self, **options):
 
+		ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)  # have to deactivate sertificate check
+		ldap.set_option(ldap.OPT_PROTOCOL_VERSION, 3)
 		l = ldap.initialize(os.environ["KARTOTEKET_LDAPSERVER"])
+		l.set_option(ldap.OPT_REFERRALS, 0)
 		l.bind_s(os.environ["KARTOTEKET_LDAPUSER"], os.environ["KARTOTEKET_LDAPPASSWORD"])
 
 		ldap_path = "DC=oslofelles,DC=oslo,DC=kommune,DC=no"
