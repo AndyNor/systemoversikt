cb5c03e94bc54f35bc158343b239df55eb5c6948
André Nordbø <andre.nordbo@gmail.com>
2022-10-06 17:52:01 +0200

    Update tmp_test_5kgrense.py

--- systemoversikt/management/commands/tmp_test_5kgrense.py
+++ systemoversikt/management/commands/tmp_test_5kgrense.py
@@ -5,6 +5,8 @@ Hensikten med denne koden er å fikse tilknytning virksomhet for DRIFT-brukere
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from systemoversikt.models import *
+import ldap
+
 
 class Command(BaseCommand):
 	def handle(self, **options):
