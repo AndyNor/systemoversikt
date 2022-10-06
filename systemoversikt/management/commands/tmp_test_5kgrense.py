1de27456076beb858e9bbf8e6153094f0b3b9b9e
André Nordbø <andre.nordbo@gmail.com>
2022-10-06 19:58:39 +0200

    Update tmp_test_5kgrense.py

--- systemoversikt/management/commands/tmp_test_5kgrense.py
+++ systemoversikt/management/commands/tmp_test_5kgrense.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
 
 		ldap_path = "DC=oslofelles,DC=oslo,DC=kommune,DC=no"
 		ldap_filter = '(&(objectCategory=Group)(cn=DS-SYE_APP_VIRK_GERICA))'
-		ldap_properties = ['member;range=5000-5020']
+		ldap_properties = ['member;range=5000-9999']
 
 		query_result = l.search_s(
 				ldap_path,
