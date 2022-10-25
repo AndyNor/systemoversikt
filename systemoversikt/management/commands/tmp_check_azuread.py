8db23ef6192f25adcaa90e23de5218537d5047c5
André Nordbø <andre.nordbo@gmail.com>
2022-10-25 11:18:20 +0200

    Create tmp_check_azuread.py

---
+++ systemoversikt/management/commands/tmp_check_azuread.py
@@ -0,0 +1,22 @@
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+import os
+import requests
+
+from django.conf import settings
+from systemoversikt.models import *
+
+
+class Command(BaseCommand):
+	def handle(self, **options):
+
+		response_jwks = requests.get(
+			"https://login.microsoftonline.com/common/discovery/v2.0/keys",
+			verify=True,
+			timeout=5,
+			proxies=None
+		)
+		response_jwks.raise_for_status()
+		jwks = response_jwks.json()
+		print(jwks)
