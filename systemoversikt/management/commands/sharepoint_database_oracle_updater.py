f45d7913a72f0ab45336d0f2ad89c5342f4ec687
André Nordbø <andre.nordbo@gmail.com>
2022-11-11 16:08:21 +0100

    Update sharepoint_database_oracle_updater.py

--- systemoversikt/management/commands/sharepoint_database_oracle_updater.py
+++ systemoversikt/management/commands/sharepoint_database_oracle_updater.py
@@ -20,13 +20,13 @@ class Command(BaseCommand):
 		source_filepath = "https://oslokommune.sharepoint.com/:x:/r/sites/74722/Begrensede-dokumenter/OK_db_oracle%20.xlsx"
 		source_file = sp.create_link(source_filepath)
 		destination_file = 'systemoversikt/import/OK_db_oracle.xlsx'
-
 		sp.download(sharepoint_location = source_file, local_location = destination_file)
 
 		@transaction.atomic
 		def import_cmdb_databases_oracle():
 
 			db_dropped = 0
+			db_servermissing = 0
 
 			#temporary file with oracle disk size (no process of data update!)
 			#print("Laster inn manuell fil med databasestørrelser:")
@@ -37,17 +37,19 @@ class Command(BaseCommand):
 			#size_data = {"%s@%s" % (item["SID"], item["Server Name"]):item["Bytes Used (GB)"] for item in size_data}
 
 			if ".xlsx" in destination_file:
-				dfRaw = pd.read_excel(destination_file)
+				dfRaw = pd.read_excel(destination_file, engine="openpyxl")
 				dfRaw = dfRaw.replace(np.nan, '', regex=True)
 				data = dfRaw.to_dict('records')
 
 			if data == None:
-				print("Problemer med innlasting fra SharePoint")
+				print("Problemer med innlasting fra SharePoint: Oracledatabaser")
 				return
+
 
 			print("OK")
 			antall_records = len(data)
 			all_existing_db = list(CMDBdatabase.objects.filter(Q(db_version__startswith="Oracle")))
+
 
 			print("Alt lastet, oppdaterer databasen:")
 			for idx, record in enumerate(data):
@@ -97,8 +99,13 @@ class Command(BaseCommand):
 
 
 				if db_server != "":
-					cmdbdevice = CMDBdevice.objects.get(comp_name=db_server)
-					cmdb_db.db_server_modelref = cmdbdevice
+					try:
+						cmdbdevice = CMDBdevice.objects.get(comp_name=db_server)
+						cmdb_db.db_server_modelref = cmdbdevice
+					except:
+						print("Feilet for %s" % db_server)
+						db_servermissing += 1
+						pass
 
 				cmdb_db.db_used_for = record["Used for"]
 				cmdb_db.db_comments = record["Comments"]
@@ -113,6 +120,50 @@ class Command(BaseCommand):
 					pass
 				cmdb_db.save()
 
+			# overskrive størrelser på databaser fra egen eksportfil
+			#try:
+			source_filepath = "https://oslokommune.sharepoint.com/:x:/r/sites/74722/Begrensede-dokumenter/oracle_database_size.xlsx"
+			source_file = sp.create_link(source_filepath)
+			oracle_size_file = 'systemoversikt/import/oracle_database_size.xlsx'
+			sp.download(sharepoint_location = source_file, local_location = oracle_size_file)
+			print("Lastet ned separat oversikt over filstørrelser..")
+
+			dfRaw = pd.read_excel(oracle_size_file, sheet_name='size_database_IZ')
+			dfRaw = dfRaw.replace(np.nan, '', regex=True)
+			oracle_size_is = dfRaw.to_dict('records')
+
+			dfRaw = pd.read_excel(oracle_size_file, sheet_name='size_database_SZ')
+			dfRaw = dfRaw.replace(np.nan, '', regex=True)
+			oracle_size_ss = dfRaw.to_dict('records')
+
+
+			def update_size(source):
+				for idx, record in enumerate(source):
+					try:
+						server = record["Host name"]
+					except:
+						server = record["Host Name"]
+					database_name = record["Database Name"]
+					size = int(record["Size (GB)"] * 1024 * 1024 * 1024)
+					for oracle_db in CMDBdatabase.objects.filter(db_database__icontains=database_name):
+						if oracle_db.db_server in server:
+							# databasenavn og server stemmer, oppdater størrelse
+							old_size = oracle_db.db_u_datafilessizekb
+							oracle_db.db_u_datafilessizekb = size
+							oracle_db.save()
+							print("Oppdaterte server %s database %s fra %s til %s bytes" % (server, database_name, old_size, size))
+
+			update_size(oracle_size_is)
+			update_size(oracle_size_ss)
+
+					#except:
+					#	print("feilet å lese inn %s" % record)
+					#	pass
+			#except:
+				#print("Kunne ikke laste ned separat oversikt over filstørrelser...")
+				#pass
+
+
 
 			# cleanup
 			obsolete_devices = all_existing_db
@@ -120,9 +171,10 @@ class Command(BaseCommand):
 			for item in obsolete_devices:
 				item.delete()
 
-			logg_entry_message = 'Fant %s databaser. %s manglet navn. Slettet %s inaktive databaser.' % (
+			logg_entry_message = 'Fant %s databaser. %s manglet navn. %s manglet serverknytning. Slettet %s inaktive databaser.' % (
 						antall_records,
 						db_dropped,
+						db_servermissing,
 						len(obsolete_devices)
 						)
 			logg_entry = ApplicationLog.objects.create(
