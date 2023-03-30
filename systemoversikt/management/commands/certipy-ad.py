ff1e2a94969e818dc07bff9576ed768f994a4a07
André Nordbø <andre.nordbo@gmail.com>
2023-03-30 17:43:05 +0200

    Update certipy-ad.py

--- systemoversikt/management/commands/certipy-ad.py
+++ systemoversikt/management/commands/certipy-ad.py
@@ -10,6 +10,10 @@ import subprocess
 
 class Command(BaseCommand):
 	def handle(self, **options):
-		command = f"certipy find -json -stdout -dc-only -u {os.environ['KARTOTEKET_LDAPUSER']} -p {os.environ['KARTOTEKET_LDAPPASSWORD']} -target {os.environ["KARTOTEKET_LDAPSERVER"]} -enabled -vulnerable -timeout 240"
+		username = os.environ['KARTOTEKET_LDAPUSER']
+		password = os.environ['KARTOTEKET_LDAPPASSWORD']
+		ldap_server = os.environ["KARTOTEKET_LDAPSERVER"]
+
+		command = f"certipy find -json -stdout -dc-only -u {username} -p {password} -target {ldap_server} -enabled -vulnerable -timeout 240"
 		result = subprocess.check_output(command, shell=True)
 		print(result)
