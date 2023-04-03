ff97335e57109fa538b8857283e28eae1b851577
André Nordbø <andre.nordbo@gmail.com>
2023-04-03 21:48:25 +0200

    Update sharepoint_database_oracle_updater.py

--- systemoversikt/management/commands/sharepoint_database_oracle_updater.py
+++ systemoversikt/management/commands/sharepoint_database_oracle_updater.py
@@ -23,19 +23,8 @@ class Command(BaseCommand):
 		destination_file = 'systemoversikt/import/OK_db_oracle.xlsx'
 		sp.download(sharepoint_location = source_file, local_location = destination_file)
 
-		#@transaction.atomic
-		#def import_cmdb_databases_oracle():
-
 		db_dropped = 0
 		db_servermissing = 0
-
-		#temporary file with oracle disk size (no process of data update!)
-		#print("Laster inn manuell fil med databasestørrelser:")
-		#filepath_size = "systemoversikt/import/oracle_disksize.xlsx"
-		#dfRaw = pd.read_excel(filepath_size)
-		#dfRaw = dfRaw.replace(np.nan, '', regex=True)
-		#size_data = dfRaw.to_dict('records')
-		#size_data = {"%s@%s" % (item["SID"], item["Server Name"]):item["Bytes Used (GB)"] for item in size_data}
 
 		if ".xlsx" in destination_file:
 			dfRaw = pd.read_excel(destination_file, engine="openpyxl")
@@ -55,8 +44,6 @@ class Command(BaseCommand):
 		print("Alt lastet, oppdaterer databasen:")
 		for idx, record in enumerate(data):
 			print(".", end="", flush=True)
-			#if idx % 1000 == 0:
-			#	print("\n%s av %s" % (idx, antall_records))
 
 			try:
 				db_fullname = record["Name"] # det er to felt som heter "name" og dette er det første...
@@ -65,7 +52,6 @@ class Command(BaseCommand):
 			except:
 				db_dropped += 1
 				continue # hvis dette ikke går er navnet feilformattert.
-			#print(db_name)
 			if db_name == "":
 				print("Database mangler navn")
 				db_dropped += 1
@@ -90,14 +76,6 @@ class Command(BaseCommand):
 
 			cmdb_db.db_version = "Oracle " + record["Version"]
 
-			#try:
-			#	size_bytes = int(size_data[db_fullname]) * 1024 * 1024 * 1024  # kommer som string i GB.
-			#	cmdb_db.db_u_datafilessizekb = size_bytes
-			#	#print(size_bytes)
-			#except:
-			#	#print("failed")
-			#	cmdb_db.db_u_datafilessizekb = 0
-
 
 			if db_server != "":
 				try:
@@ -111,7 +89,7 @@ class Command(BaseCommand):
 			cmdb_db.db_used_for = record["Used for"]
 			cmdb_db.db_comments = record["Comments"]
 			cmdb_db.billable = record["Billable"]
-			cmdb_db.db_status = record["Status"]
+			cmdb_db.db_status = record["Install Status"]
 
 			cmdb_db.sub_name = None  # reset old lookups
 			try:
@@ -139,70 +117,51 @@ class Command(BaseCommand):
 		sp.download(sharepoint_location = source_file, local_location = oracle_size_file)
 		print("Lastet ned separat oversikt over filstørrelser..")
 
-		dfRaw = pd.read_excel(oracle_size_file, sheet_name='size_database_IZ')
+		dfRaw = pd.read_excel(oracle_size_file, sheet_name='EXTERNAL_ZONE_SIZES')
+		dfRaw = dfRaw.replace(np.nan, '', regex=True)
+		oracle_size_ez = dfRaw.to_dict('records')
+
+		dfRaw = pd.read_excel(oracle_size_file, sheet_name='INTERNAL_ZONE_SIZES')
 		dfRaw = dfRaw.replace(np.nan, '', regex=True)
 		oracle_size_is = dfRaw.to_dict('records')
 
-		dfRaw = pd.read_excel(oracle_size_file, sheet_name='size_database_SZ')
-		dfRaw = dfRaw.replace(np.nan, '', regex=True)
-		oracle_size_ss = dfRaw.to_dict('records')
+		oracle_sizes = oracle_size_ez + oracle_size_is
 
-		oracle_size = oracle_size_is + oracle_size_ss
+		antall_endret = 0
+		antall_ingen_endring = 0
+		antall_feilet = 0
+		feilet_for = []
 
 		database_size_not_found = []
-		for oracle_db in CMDBdatabase.objects.filter(db_version__icontains="Oracle").filter(db_operational_status=True):
-			for idx, record in enumerate(oracle_size):
-				try:
-					server = record["Host name"]
-				except:
-					server = record["Host Name"]
+		for idx, record in enumerate(oracle_sizes):
+			import_databasenavn = record["TARGET_NAME"].strip()
+			import_server = record["HOST_NAME"].strip().split(".")[0]
+			try:
+				dbinstance = CMDBdatabase.objects.get(db_database=import_databasenavn,db_server=import_server)
+			except:
+				#print(f"No matching database with name {import_databasenavn} for server {import_server}")
+				antall_feilet += 1
+				feilet_for.append("%s@%s" % (import_databasenavn, import_server))
+				continue
 
-				server = re.search(r'^([^.]*).*$', server, re.I)[0]
-				database_name = re.search(r'^([a-zA-Z0-9]*)', record["Database Name"], re.I)[0]
-
-				if (oracle_db.db_database.lower() == database_name.lower()) and (oracle_db.db_server.lower() in server.lower()):
-					# databases are often set up redundant. Same size on both serveres.
-					# databasenavn og server stemmer, oppdater størrelse
-
-					new_size = int(record["Size (GB)"] * 1000 * 1000 * 1000)
-					old_size = oracle_db.db_u_datafilessizekb
-					#print("%s = %s" % (old_size, size))
-					if old_size != new_size:
-						oracle_db.db_u_datafilessizekb = new_size
-
-
-
-						#oracle_db.save()
+			new_size = int(record["DATABASE SIZE IN GB"] * 1000 * 1000 * 1000)
+			old_size = dbinstance.db_u_datafilessizekb
+			if old_size != new_size:
+				dbinstance.db_u_datafilessizekb = new_size
+				dbinstance.save()
+				print(f"Oppdaterte størrelse på {import_databasenavn}@{import_server} fra {old_size} til {new_size}")
+				antall_endret += 1
+			else:
+				#print(f"Ingen endring på {import_databasenavn}")
+				antall_ingen_endring += 1
 
 
-
-
-						#print(oracle_db.db_u_datafilessizekb)
-						print("Oppdaterer %s@%s (%s@%s) fra %s til %s bytes" % (oracle_db.db_database, oracle_db.db_server, database_name, server, old_size, new_size))
-						break
-
-			# loop complete, did not find any match
-			database_size_not_found.append(oracle_db)
-
-		for oracle_db in database_size_not_found:
-			pass
-			#print("Fant ikke data på %s@%s" % (oracle_db.db_database, oracle_db.db_server))
-
-
-
-		logg_entry_message = 'Fant %s databaser. %s manglet navn. %s manglet serverknytning. Slettet %s inaktive databaser. %s mangler størrelse' % (
-					antall_records,
-					db_dropped,
-					db_servermissing,
-					len(obsolete_devices),
-					len(database_size_not_found),
-					)
+		logg_entry_message = f"Oppdaterte størrelser på Orcale-databaser. {antall_endret} endret. {antall_ingen_endring} som før. {antall_feilet} oppslag feilet: {feilet_for}"
 		logg_entry = ApplicationLog.objects.create(
 				event_type='CMDB database import (Oracle)',
 				message=logg_entry_message,
 			)
-		print("\n")
-		print(logg_entry_message)
+		print(f"\n {logg_entry_message}")
 
 
 		#eksekver
