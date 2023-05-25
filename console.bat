f4e3cbde58e4e730fc8bf03317cf82c11a324e53
André Nordbø <andre.nordbo@gmail.com>
2023-05-25 09:32:00 +0200

    sqlite3 for win lagt inn

--- console.bat
+++ console.bat
@@ -1,2 +1,3 @@
 @echo off
+SET PATH=C:\Utvikling\kildekode\systemoversikt\sqlite3_win
 cmd /k "cd /d C:\Utvikling\virtualenv\kartoteket\Scripts & activate & cd /d C:\Utvikling\kildekode\systemoversikt"
---
+++ systemoversikt/management/commands/open_relay.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+import smtplib
+
+class Command(BaseCommand):
+	def handle(self, **options):
+
+		target_server = 'fill inn'
+		target_port = 25
+		sender = "fill inn"
+		receivers = ["fill inn",]
+		message = f"test av apent rele mot {target_server}."
+
+		smtpObj = smtplib.SMTP(target_server, target_port)
+		smtpObj.sendmail(sender, receivers, message)
+		smtpObj.quit()
