0fae5407492532dc1aaeb0041c980536fd7c9379
André Nordbø <andre.nordbo@gmail.com>
2023-09-04 14:03:50 +0200

    Update azure_graph_named_locations.py

--- systemoversikt/management/commands/azure_graph_named_locations.py
+++ systemoversikt/management/commands/azure_graph_named_locations.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+import io
 import os
 import simplejson as json
 from azure.identity import ClientSecretCredential
@@ -7,6 +8,10 @@ from systemoversikt.models import *
 
 class Command(BaseCommand):
 	def handle(self, **options):
+
+		f = io.open("systemoversikt/management/commands/iso-3166-2.json", mode="r", encoding="utf-8")
+		content = f.read()
+		countrycodes = json.loads(content)
 
 		client_credential = ClientSecretCredential(
 				tenant_id=os.environ['AZURE_TENANT_ID'],
@@ -17,5 +22,17 @@ class Command(BaseCommand):
 		client = GraphClient(credential=client_credential, api_version='beta')
 		query = "/identity/conditionalAccess/namedLocations"
 		resp = client.get(query)
-		load_appdata = json.loads(resp.text)
-		print(json.dumps(load_appdata, sort_keys=True, indent=4))
+		json_data = json.loads(resp.text)
+
+		print(json.dumps(json_data, sort_keys=True, indent=4))
+
+		for named_location in json_data["value"]:
+			print(named_location["displayName"])
+			if "countriesAndRegions" in named_location:
+				for code in named_location["countriesAndRegions"]:
+					if code in countrycodes:
+						print("** " + countrycodes[code])
+					else:
+						print(f"** Ukjent kode: {code}")
+
+
