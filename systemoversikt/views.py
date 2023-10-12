79b2ae90e9e99fc05c866775ca28fc2bf23c3696
André Nordbø <andre.nordbo@gmail.com>
2023-10-12 14:12:36 +0200

    Update views.py

--- systemoversikt/views.py
+++ systemoversikt/views.py
@@ -2614,7 +2614,7 @@ def home(request):
 	nyeste_programvarer = Programvare.objects.order_by('-pk')[:10]
 	antall_behandlinger = BehandlingerPersonopplysninger.objects.count()
 	kategorier = SystemKategori.objects.all()
-	nyheter = NyeFunksjoner.objects.all().order_by('-tidspunkt')[:5]
+	nyheter = NyeFunksjoner.objects.all().order_by('-tidspunkt')[:10]
 
 	return render(request, 'site_home.html', {
 		'request': request,
