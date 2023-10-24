6d8308a4a2982af6e5155e29468f5fc4f4d13fc8
André Nordbø <andre.nordbo@gmail.com>
2023-10-24 09:44:38 +0200

    Update views.py

--- systemoversikt/views.py
+++ systemoversikt/views.py
@@ -4367,7 +4367,7 @@ def virksomhet(request, pk):
 		parents.append('Ingen')
 		return 'Ingen'
 
-	antall_graph_noder = System.objects.filter(systemforvalter=pk).(filter(~Q(livslop_status__in=[6,7]))).count()
+	antall_graph_noder = System.objects.filter(systemforvalter=pk).filter(~Q(livslop_status__in=[6,7])).count()
 	for system in System.objects.filter(systemforvalter=pk).filter(~Q(livslop_status__in=[6,7])).order_by('systemnavn'):
 		if system.er_ibruk():
 			nodes.append({
