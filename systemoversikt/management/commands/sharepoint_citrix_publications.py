8a7d17a2744b9b5501047ea7caf8043da7a3599b
André Nordbø <andre.nordbo@gmail.com>
2024-03-20 09:19:28 +0100

    desktopgroups or silos added

--- systemoversikt/management/commands/sharepoint_citrix_publications.py
+++ systemoversikt/management/commands/sharepoint_citrix_publications.py
@@ -12,14 +12,19 @@ from systemoversikt.views import sharepoint_get_file
 class Command(BaseCommand):
 	def handle(self, **options):
 
-		skip_sp = False # Sett til True ved lokal testing
+		skip_sharepoint = False # Sett til True ved lokal testing
 
 		INTEGRASJON_KODEORD = "sp_citrix"
 		LOG_EVENT_TYPE = "Citrix publikasjon"
 		KILDE = "Citrix"
 		PROTOKOLL = "Manuelt"
 		BESKRIVELSE = "Citrixpubliseringer fra intern og sikker sone"
-		FILNAVN = {"citrix_is": "citrix_publikasjoner_is.json", "citrix_ss": "citrix_publikasjoner_ss.json"}
+		FILNAVN = {
+				"citrix_is": "citrix_publikasjoner_is.json",
+				"citrix_is_desktop_gr": "citrix_BrokerDesktopGroups_is.json",
+				"citrix_ss": "citrix_publikasjoner_ss.json",
+				"citrix_ss_desktop_gr": "citrix_BrokerDesktopGroups_ss.json",
+			}
 		URL = ""
 		FREKVENS = "Manuelt"
 
@@ -56,32 +61,51 @@ class Command(BaseCommand):
 
 			sp_citrix_is = FILNAVN["citrix_is"]
 			sp_citrix_ss = FILNAVN["citrix_ss"]
+			sp_citrix_is_desktop_gr = FILNAVN["citrix_is_desktop_gr"]
+			sp_citrix_ss_desktop_gr = FILNAVN["citrix_ss_desktop_gr"]
 
-			if skip_sp:
+			if skip_sharepoint:
 				citrix_is_lokalfil, citrix_is_date = ("systemoversikt/import/citrix_publikasjoner_is.json", None)
 				citrix_ss_lokalfil, citrix_ss_date = ("systemoversikt/import/citrix_publikasjoner_ss.json", None)
+				citrix_is_desktop_gr_lokalfil, citrix_is_desktop_gr_date = ("systemoversikt/import/citrix_BrokerDesktopGroups_is.json", None)
+				citrix_ss_desktop_gr_lokalfil, citrix_ss_desktop_gr_date = ("systemoversikt/import/citrix_BrokerDesktopGroups_ss.json", None)
 			else:
 				citrix_is_lokalfil, citrix_is_date = hent_fil(sp_citrix_is)
 				citrix_ss_lokalfil, citrix_ss_date = hent_fil(sp_citrix_ss)
+				citrix_is_desktop_gr_lokalfil, citrix_is_desktop_gr_date = hent_fil(sp_citrix_is_desktop_gr)
+				citrix_ss_desktop_gr_lokalfil, citrix_ss_desktop_gr_date = hent_fil(sp_citrix_ss_desktop_gr)
 
 			logg_entry_message = ""
 
-			print(citrix_is_lokalfil)
-			print(citrix_ss_lokalfil)
-
 			@transaction.atomic
-			def import_citrix(filnavn, filsti, dato, zone):
+			def import_citrix(filnavn, filsti, dato, zone, desktop_groups_sti):
 
 				with open(filsti, 'r', encoding="utf-8") as f:
 					#print(f"Encoding er {f.encoding}") # de originale filene var "cp1252", men er konvertert via Sublime text editor
 					data = f.read()#.encode('latin1').decode('cp1252').encode('utf-8')
 
+				with open(desktop_groups_sti, 'r', encoding="utf-8") as f:
+					desktop_gr_data = f.read()
+
 				json_data = json.loads(data)
+				json_desktop_gr = json.loads(desktop_gr_data)
 				antall_records = len(json_data)
 
 				for line in json_data:
 					c, created = CitrixPublication.objects.get_or_create(publikasjon_UUID=line['UUID'])
 					c.sone = zone
+
+					AllAssociatedDesktopGroupUids_Name = []
+					for desktopgroup in line['AllAssociatedDesktopGroupUids']:
+						oppslag = None
+						for obj in json_desktop_gr:
+							if obj["Uid"] == desktopgroup:
+								oppslag = obj["DesktopGroupName"]
+						#print(f"{desktopgroup} --> {oppslag}")
+						AllAssociatedDesktopGroupUids_Name.append(oppslag)
+
+					line["AllAssociatedDesktopGroupUids_Name"] = AllAssociatedDesktopGroupUids_Name
+
 					c.publikasjon_json = json.dumps(line)
 					c.publikasjon_active = line['Enabled']
 					c.save()
@@ -93,9 +117,9 @@ class Command(BaseCommand):
 
 			#eksekver
 			print(f"Importerer fra {sp_citrix_is}")
-			logg_entry_message += import_citrix(sp_citrix_is, citrix_is_lokalfil, citrix_is_date, "Intern")
+			logg_entry_message += import_citrix(sp_citrix_is, citrix_is_lokalfil, citrix_is_date, "Intern", citrix_is_desktop_gr_lokalfil)
 			print(f"Importerer fra {sp_citrix_ss}")
-			logg_entry_message += import_citrix(sp_citrix_ss, citrix_ss_lokalfil, citrix_ss_date, "Sikker")
+			logg_entry_message += import_citrix(sp_citrix_ss, citrix_ss_lokalfil, citrix_ss_date, "Sikker", citrix_ss_desktop_gr_lokalfil)
 
 
 			# lagre sist oppdatert tidspunkt
