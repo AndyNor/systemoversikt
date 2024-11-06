82587bbfd0f7d1f89807744f181f8d9484fc7a36
André Nordbø <andre.nordbo@gmail.com>
2024-11-06 14:37:50 +0100

    Update ldap_users_paged.py

--- systemoversikt/management/commands/ldap_users_paged.py
+++ systemoversikt/management/commands/ldap_users_paged.py
@@ -62,7 +62,7 @@ class Command(BaseCommand):
 			SEARCHFILTER = '(&(objectCategory=person)(objectClass=user))'
 			LDAP_SCOPE = ldap.SCOPE_SUBTREE
 			ATTRLIST = ['objectSid', 'cn', 'givenName', 'sn', 'userAccountControl', 'mail', 'msDS-UserPasswordExpiryTimeComputed', 'description', 'displayName', 'sAMAccountName', 'lastLogonTimestamp', 'whenCreated', 'pwdLastSet', 'servicePrincipalName'] # if empty we get all attr we have access to
-			PAGESIZE = 500
+			PAGESIZE = 2000
 
 			report_data = {
 				"created": 0,
@@ -203,16 +203,16 @@ class Command(BaseCommand):
 					last_name = hashlib.sha256(last_name.encode('utf-8')).hexdigest()[0:24]
 				user.last_name = last_name
 
-				#try:
-				objectsid = attrs["objectSid"]
-				if len(objectsid) == 0:
-					print(f"ObjectSid contains no elements for {user}")
-				else:
-					user.object_sid = decode_sid(objectsid[0])
-					if len(objectsid) > 1:
-						print(f"ObjectSid contains more than one element for {user}")
-				#except:
-				#	print(f"ObjectSid failed for {user}")
+				try:
+					objectsid = attrs["objectSid"]
+					if len(objectsid) == 0:
+						print(f"ObjectSid contains no elements for {user}")
+					else:
+						user.profile.object_sid = decode_sid(objectsid[0])
+						if len(objectsid) > 1:
+							print(f"ObjectSid contains more than one element for {user}")
+				except:
+					print(f"ObjectSid failed for {user}")
 
 
 				email = ""
