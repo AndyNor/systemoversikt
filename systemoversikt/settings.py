562b02a689017f6c985bf12e8e604561d67d7861
André Nordbø <andre.nordbo@gmail.com>
2025-01-30 18:39:17 +0100

    Update settings.py

--- systemoversikt/settings.py
+++ systemoversikt/settings.py
@@ -178,7 +178,7 @@ if THIS_ENVIRONMENT == "PROD":
 			'NAME': 'kartoteket',
 			'USER': os.environ["POSTGRES_USER"],
 			'PASSWORD': os.environ["POSTGRES_PASSWORD"],
-			'HOST': '127.0.0.1',
+			'HOST': 'localhost',
 			'PORT': '5432',
 		}
 	}
