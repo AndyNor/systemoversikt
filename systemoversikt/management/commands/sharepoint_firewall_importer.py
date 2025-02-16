8fff21a4d97c06e0f4e465d199e62bd71fc24665
André Nordbø <andre.nordbo@gmail.com>
2025-02-16 17:11:20 +0100

    Update sharepoint_firewall_importer.py

--- systemoversikt/management/commands/sharepoint_firewall_importer.py
+++ systemoversikt/management/commands/sharepoint_firewall_importer.py
@@ -226,11 +226,11 @@ class Command(BaseCommand):
 			cmdb_server_buffer = {}
 			all_cmdb_servers = CMDBdevice.objects.filter(device_type="SERVER") #.values("comp_ip_address", "pk", "comp_name", "tjenestenavn")
 			for server in all_cmdb_servers:
-				server_ip = server["comp_ip_address"]
+				server_ip = server.comp_ip_address
 				if server_ip == "":
 					continue
 				if not server_ip in cmdb_server_buffer:
-					cmdb_server_buffer[server_ip] = {"pk": server["pk"], "comp_name": server['comp_name'], "sub_name": server.offering_navn()}
+					cmdb_server_buffer[server_ip] = {"pk": server.pk, "comp_name": server.comp_name, "sub_name": server.offering_navn()}
 				else:
 					#print(f"Server {server} kunne ikke legges til flere ganger")
 					continue
