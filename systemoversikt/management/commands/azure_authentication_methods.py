3b4f0c2a598d71e9243e4b224b5327aeb99b6290
André Nordbø <andre.nordbo@gmail.com>
2025-02-27 21:39:30 +0100

    Update azure_authentication_methods.py

--- systemoversikt/management/commands/azure_authentication_methods.py
+++ systemoversikt/management/commands/azure_authentication_methods.py
@@ -24,7 +24,7 @@ class Command(BaseCommand):
 
 	SLEEP_BETWEEN = 0
 	SLEEP_TOO_MANY = 20
-	ITEMS_PER_DAY = 10
+	ITEMS_PER_DAY = 600
 
 	def handle(self, **options):
 
@@ -79,7 +79,7 @@ class Command(BaseCommand):
 			def create_batch_request(users):
 				requests = []
 				for i, user in enumerate(users):
-					print(f"{user} {user.profile.auth_methods_last_update} {user.profile.entra_id_auth()}")
+					#print(f"{user} {user.profile.auth_methods_last_update} {user.profile.entra_id_auth()}")
 					upn = user.email
 					requests.append({
 						"id": i,
