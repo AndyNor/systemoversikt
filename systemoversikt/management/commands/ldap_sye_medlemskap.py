18f6756621518a5c0e1798bca4a0d6fa703a9a18
André Nordbø <andre.nordbo@gmail.com>
2025-02-28 00:54:15 +0100

    Update ldap_sye_medlemskap.py

--- systemoversikt/management/commands/ldap_sye_medlemskap.py
+++ systemoversikt/management/commands/ldap_sye_medlemskap.py
@@ -59,13 +59,14 @@ class Command(BaseCommand):
 					try:
 						adg = ADgroup.objects.get(distinguishedname=g)
 						if adg.mail:
+							print(f"Legger til {adg} for {bruker}")
 							bruker.mail_enabled_groups.add(adg)
-						print(f"Fant gruppen {adg}")
+						#print(f"Fant gruppen {adg}")
 						#bruker.profile.adgrupper.add(adg)
 						#bruker.profile.adgrupper_antall = len(grupper)
 						#bruker.profile.save() # Det er ikke behov for å lagre når en legger til ting
 					except:
-						print("Error, fant ikke %s" % (g))
+						#print("Error, fant ikke %s" % (g))
 						pass
 
 			#logg dersom vellykket
