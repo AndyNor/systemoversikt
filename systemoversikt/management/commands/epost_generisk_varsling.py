467cc502b45f32fc31461f5fc3d60e406b663758
André Nordbø <andre.nordbo@gmail.com>
2025-08-31 12:06:07 +0200

    Update epost_generisk_varsling.py

--- systemoversikt/management/commands/epost_generisk_varsling.py
+++ systemoversikt/management/commands/epost_generisk_varsling.py
@@ -11,7 +11,7 @@ from django.core.mail import EmailMessage
 
 class Command(BaseCommand):
 
-	UTSENDINGSDAG = 1  # dag av måned
+	UTSENDINGSDAG = 31  # dag av måned
 
 	def handle(self, **options):
 
@@ -60,14 +60,13 @@ class Command(BaseCommand):
 				for virksomhet in Virksomhet.objects.filter(ordinar_virksomhet=True):
 
 					subject = "Kartoteket: Påminnelse om ansatte med ansvar som har sluttet"
-
 					recipients = [ansvarlig.brukernavn.email for ansvarlig in virksomhet.ikt_kontakt.all()]
 
-					ansvarlige_systemforvaltere = Ansvarlig.objects.filter(brukernavn__profile__virksomhet=virksomhet).filter(~Q(system_systemforvalter_kontaktpersoner=None))
+					ansvarlige_systemforvaltere = Ansvarlig.objects.filter(brukernavn__profile__virksomhet=virksomhet).filter(~Q(system_forvalter_for=None))
 					ansvarlige_systemforvaltere_html = "".join([f"\n<li><a href=\"https://kartoteket.oslo.kommune.no/brukere/ad/{ansvarlig.brukernavn.pk}/\">{ansvarlig.brukernavn}</a></li>" for ansvarlig in ansvarlige_systemforvaltere])
 					ansvarlige_systemforvaltere_html = f"<p>Deaktive personer angitt som systemforvalter:\n<ol>{ansvarlige_systemforvaltere_html}\n</ol></p>" if len(ansvarlige_systemforvaltere) > 0 else "<p>Dere har ingen deaktiverte systemforvaltere. Godt jobbet!</p>"
 
-					ansvarlige_lokale_systemforvaltere = Ansvarlig.objects.filter(brukernavn__profile__virksomhet=virksomhet).filter(~Q(systembruk_systemforvalter_kontaktpersoner=None))
+					ansvarlige_lokale_systemforvaltere = Ansvarlig.objects.filter(brukernavn__profile__virksomhet=virksomhet).filter(~Q(systembruk_forvalter_for=None))
 					ansvarlige_lokale_systemforvaltere_html = "".join([f"\n<li><a href=\"https://kartoteket.oslo.kommune.no/brukere/ad/{ansvarlig.brukernavn.pk}/\">{ansvarlig.brukernavn}</a></li>" for ansvarlig in ansvarlige_lokale_systemforvaltere])
 					ansvarlige_lokale_systemforvaltere_html = f"\n<p>Deaktive personer angitt som lokal systemforvalter:\n<ol>{ansvarlige_lokale_systemforvaltere_html}\n</ol></p>" if len(ansvarlige_lokale_systemforvaltere) > 0 else "<p>Dere har ingen deaktiverte lokale systemforvaltere. Godt jobbet!</p>"
 
@@ -77,24 +76,14 @@ class Command(BaseCommand):
 
 					message = f"""
 Hei IKT-hovedkontakter i {virksomhet.virksomhetsforkortelse},
-
 <p>Denne e-posten går ut en gang per måned</p>
-
 {ansvarlige_systemforvaltere_html}
-
 {ansvarlige_lokale_systemforvaltere_html}
-
 <p>For en oversikt over alle ansvarlige for din virksomhet kan du gå til <a href="https://kartoteket.oslo.kommune.no/virksomhet/ansvarlige/{virksomhet.pk}/">https://kartoteket.oslo.kommune.no/virksomhet/ansvarlige/{virksomhet.pk}/</a></p>
-
 <p>Hver natt kjøres det en bakgrunnsjobb i Kartoteket for å slette ansvarlige som ikke lenger har roller eller ansvar.</p>
-
 <p>Husk også å sjekke om virksomhetens kontaktpersoner er oppdatert på https://kartoteket.oslo.kommune.no/virksomhet/min/ via knappen "Rediger virksomhetsdetaljer og kontaktpersoner".</p>
-
-<br><br>Vennlig hilsen<br>
-Kartoteket
+<br><br>Vennlig hilsen<br>Kartoteket
 """
-
-
 					email = EmailMessage(
 							subject=subject,
 							body=message,
@@ -103,15 +92,57 @@ Kartoteket
 					)
 					email.content_subtype = "html"
 					if recipients:
-						#print(f"E-post er lagt til kø for utsending til {virksomhet.virksomhetsforkortelse}")
-						email.send()
+						print(f"E-post er lagt til kø for utsending til {virksomhet.virksomhetsforkortelse}")
+						#email.send()
 					else:
 						print(f"Kan ikke sende e-post til {virksomhet.virksomhetsforkortelse} fordi det mangler e-post på hovedkontakt")
 
 
+
+				# melding til alle systemforvaltere og lokale systemforvaltere om hva er er oppgitt som ansvarlig for samt oppfordre til å oppdatere
+				for ansvarlig in Ansvarlig.objects.filter(brukernavn__profile__accountdisable=False): # alle aktive ansvarlige
+					system_forvalter_for = ansvarlig.system_forvalter_for.all()
+					systembruk_forvalter_for = ansvarlig.systembruk_forvalter_for.all()
+
+					sum_ansvar = len(system_forvalter_for) + len(systembruk_forvalter_for)
+					if sum_ansvar < 1:
+						continue # ikke send melding og hopp til neste ansvarlige
+
+					system_forvalter_for_html = "".join([f"\n<li><a href=\"https://kartoteket.oslo.kommune.no/systemer/detaljer/{system.pk}/\">{system}</a></li>" for system in system_forvalter_for])
+					system_forvalter_for_html = f"\n<p>Systemer du er angitt som forvalter for:\n<ol>{system_forvalter_for_html}\n</ol></p>" if len(system_forvalter_for) > 0 else "<p>Du er ikke registrert som systemforvalter direkte på noe system.</p>"
+
+					systembruk_forvalter_for_html = "".join([f"\n<li><a href=\"https://kartoteket.oslo.kommune.no/systemer/bruk/{systembruk.pk}/\">{systembruk}</a></li>" for systembruk in systembruk_forvalter_for])
+					systembruk_forvalter_for_html = f"\n<p>Systemer du er angitt som lokal forvalter for:\n<ol>{systembruk_forvalter_for_html}\n</ol></p>" if len(systembruk_forvalter_for) > 0 else "<p>Du er ikke registrert som lokal systemforvalter for noe system.</p>"
+
+
+					subject = "Kartoteket: Påminnelse om dine roller i systemoversikten"
+					recipients = [ansvarlig.brukernavn.email]
+					message = f"""
+Hei {ansvarlig.brukernavn.profile.displayName},
+<p>Denne e-posten går ut en gang per måned for å minne deg om dine roller i systemoversikten.</p>
+{system_forvalter_for_html}
+{systembruk_forvalter_for_html}
+<p>Hvis du ser noe du ikke kjenner deg igjen i her, ber vi deg ta kontakt med din IKT-organisasjon for å avklare ansvaret. Du kan høre med din nærmeste leder eller eventuelt med din nærmeste IKT-hovedkontakt.</p>
+<p>Det kan hende du har flere roller i systemoversikten enn de som er listet opp her. For full oversikt kan du gå til <a href="https://kartoteket.oslo.kommune.no/brukere/ad/{ansvarlig.brukernavn.pk}/">https://kartoteket.oslo.kommune.no/brukere/ad/{ansvarlig.brukernavn.pk}/</a>.</p>
+<br><br>Vennlig hilsen<br>Kartoteket
+"""
+					print(message)
+					email = EmailMessage(
+							subject=subject,
+							body=message,
+							from_email=settings.DEFAULT_FROM_EMAIL,
+							to=recipients,
+					)
+					email.content_subtype = "html"
+					if recipients:
+						print(f"E-post er lagt til kø for utsending til {ansvarlig.brukernavn}")
+						#email.send()
+					else:
+						print(f"Kan ikke sende e-post til {ansvarlig.brukernavn} fordi det mangler e-postadresse")
+
+
+
 				logg_message = f"Klartgjort e-post om ansvarlige som har sluttet og melding til alle systemforvaltere."
-
-
 
 			else:
 				logg_message = f"Det er ikke dag {Command.UTSENDINGSDAG} av måneden og dropper derfor utsending"
