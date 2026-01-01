7043999aa7ad009f06305029b23f530bd5dacac7
André Nordbø <andre.nordbo@gmail.com>
2026-01-01 19:28:32 +0100

    nytt virtuelt miljø for python

--- windev/console.bat
+++ windev/console.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /k "cd /d C:\Utvikling\virtualenv\kartoteket\Scripts & activate & cd /d C:\Utvikling\kildekode\systemoversikt"
+cmd /k "cd /d C:\Utvikling\kildekode\systemoversikt\.venv\Scripts & activate & cd /d C:\Utvikling\kildekode\systemoversikt"
--- windev/testserver.bat
+++ windev/testserver.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /k "cd /d C:\Utvikling\virtualenv\kartoteket\Scripts & activate & cd /d C:\Utvikling\kildekode\systemoversikt & python manage.py runserver"
+cmd /k "cd /d C:\Utvikling\kildekode\systemoversikt\.venv\Scripts & activate & cd /d C:\Utvikling\kildekode\systemoversikt & python manage.py runserver"
