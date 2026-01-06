b64212fa2f5048e6f212c76348817f4174646bb5
André Nordbø <andre.nordbo@gmail.com>
2026-01-06 15:01:38 +0100

    Update manuell_sjekke_ldap.py

--- systemoversikt/management/commands/manuell_sjekke_ldap.py
+++ systemoversikt/management/commands/manuell_sjekke_ldap.py
@@ -7,57 +7,56 @@ from systemoversikt.views import ldap_query
 
 class Command(BaseCommand):
 	def handle(self, **options):
+		
+		def ldap_query_with_sd(ldap_path, ldap_filter, ldap_properties, timeout, sdflags=0x07):
+			import ldap, os
+			from ldap.controls import LDAPControl
+			from pyasn1.type.univ import Integer
+			from pyasn1.codec.ber import encoder  # <- BER-encode ASN.1 INTEGER
+
+			server = 'ldaps://ldaps.oslofelles.oslo.kommune.no:636'
+			user = os.environ["KARTOTEKET_LDAPUSER"]
+			password = os.environ["KARTOTEKET_LDAPPASSWORD"]
+
+			ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+			ldap.set_option(ldap.OPT_PROTOCOL_VERSION, 3)
+
+			ldap_client = ldap.initialize(server)
+			ldap_client.timeout = timeout
+			ldap_client.set_option(ldap.OPT_REFERRALS, 0)
+			ldap_client.bind_s(user, password)
+
+			sd_flags_oid = '1.2.840.113556.1.4.801'
+			# BER-encode the integer value per ASN.1
+			control_value = encoder.encode(Integer(sdflags))
+			# criticality can be False; True is also fine
+			sd_control = LDAPControl(sd_flags_oid, False, control_value)
+
+			result = ldap_client.search_ext_s(
+				ldap_path,
+				ldap.SCOPE_SUBTREE,
+				ldap_filter,
+				ldap_properties,
+				serverctrls=[sd_control]
+			)
+
+			ldap_client.unbind_s()
+			return result
 
 
-	def ldap_query_with_sd(ldap_path, ldap_filter, ldap_properties, timeout, sdflags=0x07):
-		import ldap, os
-		from ldap.controls import LDAPControl
-		from pyasn1.type.univ import Integer
-		from pyasn1.codec.ber import encoder  # <- BER-encode ASN.1 INTEGER
+		ldap_path = "DC=oslofelles,DC=oslo,DC=kommune,DC=no"
+		ldap_filter = ('(distinguishedName=%s)' %
+				"CN=user,OU=ServiceAccounts,OU=AD,OU=Administrasjon,DC=oslofelles,DC=oslo,DC=kommune,DC=no")
+		ldap_properties = ['cn', 'mail', 'givenName', 'displayName', 'sn',
+				'userAccountControl', 'nTSecurityDescriptor']
 
-		server = 'ldaps://ldaps.oslofelles.oslo.kommune.no:636'
-		user = os.environ["KARTOTEKET_LDAPUSER"]
-		password = os.environ["KARTOTEKET_LDAPPASSWORD"]
+		result = ldap_query_with_sd(ldap_path, ldap_filter, ldap_properties, timeout=10, sdflags=0x07)
 
-		ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-		ldap.set_option(ldap.OPT_PROTOCOL_VERSION, 3)
-
-		ldap_client = ldap.initialize(server)
-		ldap_client.timeout = timeout
-		ldap_client.set_option(ldap.OPT_REFERRALS, 0)
-		ldap_client.bind_s(user, password)
-
-		sd_flags_oid = '1.2.840.113556.1.4.801'
-		# BER-encode the integer value per ASN.1
-		control_value = encoder.encode(Integer(sdflags))
-		# criticality can be False; True is also fine
-		sd_control = LDAPControl(sd_flags_oid, False, control_value)
-
-		result = ldap_client.search_ext_s(
-			ldap_path,
-			ldap.SCOPE_SUBTREE,
-			ldap_filter,
-			ldap_properties,
-			serverctrls=[sd_control]
-		)
-
-		ldap_client.unbind_s()
-		return result
-
-
-	ldap_path = "DC=oslofelles,DC=oslo,DC=kommune,DC=no"
-	ldap_filter = ('(distinguishedName=%s)' %
-			"CN=user,OU=ServiceAccounts,OU=AD,OU=Administrasjon,DC=oslofelles,DC=oslo,DC=kommune,DC=no")
-	ldap_properties = ['cn', 'mail', 'givenName', 'displayName', 'sn',
-			'userAccountControl', 'nTSecurityDescriptor']
-
-	result = ldap_query_with_sd(ldap_path, ldap_filter, ldap_properties, timeout=10, sdflags=0x07)
-
-	for dn, attrs in result:
-		if dn:
-			for key, value in attrs.items():
-				if key == 'nTSecurityDescriptor':
-					raw_sd = value[0]
-					print(f'{key}: {len(raw_sd)} bytes')
-				else:
-					print(f'{key}: {value}\n')
+		for dn, attrs in result:
+			if dn:
+				for key, value in attrs.items():
+					if key == 'nTSecurityDescriptor':
+						raw_sd = value[0]
+						print(f'{key}: {len(raw_sd)} bytes')
+					else:
+						print(f'{key}: {value}\n')
