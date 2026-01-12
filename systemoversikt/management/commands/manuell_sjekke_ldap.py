5fabb53af6beedadb71bef2d24472dcc7582fe4b
André Nordbø <andre.nordbo@gmail.com>
2026-01-12 15:34:20 +0100

    Update manuell_sjekke_ldap.py

--- systemoversikt/management/commands/manuell_sjekke_ldap.py
+++ systemoversikt/management/commands/manuell_sjekke_ldap.py
@@ -8,88 +8,9 @@ import os
 class Command(BaseCommand):
 	def handle(self, **options):
 
-		from ldap3 import Server, Connection, ALL, SUBTREE, NTLM
-		from impacket.dcerpc.v5 import samr
-		from impacket.dcerpc.v5.dtypes import RPC_SID
-		import struct
-		import os
 
-		# --- CONFIG ---
-		ldap_host = 'ldaps.oslofelles.oslo.kommune.no'
-		username = os.environ["KARTOTEKET_LDAPUSER"]
-		password = os.environ["KARTOTEKET_LDAPPASSWORD"]
-		dn = 'CN=S-BRE-MSCRM-ADMIN,OU=ServiceAccounts,OU=AD,OU=Administrasjon,DC=oslofelles,DC=oslo,DC=kommune,DC=no'
-		base_dn = 'DC=oslofelles,DC=oslo,DC=kommune,DC=no'
-
-		# --- CONNECT ---
-		server = Server(ldap_host, port=636, use_ssl=True, get_info=ALL)
-		conn = Connection(server, user=username, password=password, authentication=NTLM)
-		if not conn.bind():
-			raise Exception("LDAP bind failed: " + str(conn.result))
-
-		# --- Request DACL only ---
-		SDFLAGS_OID = '1.2.840.113556.1.4.801'
-		DACL_ONLY = (SDFLAGS_OID, True, b'\x04\x00\x00\x00')
-
-		conn.search(dn, '(objectClass=*)', search_scope='BASE',
-					attributes=['nTSecurityDescriptor'])#, controls=[DACL_ONLY])
-
-		if not conn.entries:
-			raise SystemExit("No entries returned. Check DN or permissions.")
-
-		sd_data = conn.entries[0]['nTSecurityDescriptor'].raw_values[0]
-
-		# --- Parse DACL manually ---
-		# Security Descriptor header is 20 bytes: <BBHLLLL
-		rev, sbz1, control, owner_ofs, group_ofs, sacl_ofs, dacl_ofs = struct.unpack('<BBHLLLL', sd_data[:20])
-		if dacl_ofs == 0:
-			print("No DACL present.")
-			conn.unbind()
-			exit()
-
-		dacl = samr.DACL(sd_data[dacl_ofs:])
-
-		# SID -> name helper
-		def sid_to_name(sid_str):
-			try:
-				sid_obj = RPC_SID(); sid_obj.fromString(sid_str)
-				sid_bytes = sid_obj.getData()
-				esc = ''.join('\\{:02x}'.format(b) for b in sid_bytes)
-				flt = f'(objectSid={esc})'
-				if conn.search(base_dn, flt, SUBTREE, attributes=['sAMAccountName']):
-					if conn.entries:
-						return str(conn.entries[0]['sAMAccountName'].value)
-			except:
-				pass
-			return sid_str
-
-		RIGHTS_MAP = {
-			0x10000000: 'GenericAll',
-			0x40000000: 'GenericWrite',
-			0x80000000: 'GenericRead',
-			0x20000000: 'GenericExecute',
-			0x00000008: 'WriteDacl',
-			0x00000002: 'WriteOwner',
-		}
-
-		print(f"{'Identity':<35} {'RightsMask':<12} {'Rights':<40} {'ACEType':<10} {'Inherited'}")
-		print('-' * 100)
-		for ace in dacl.aces:
-			mask = ace['Ace']['Mask']
-			rights = [name for bit, name in RIGHTS_MAP.items() if mask & bit]
-			identity = sid_to_name(str(ace['Ace']['Sid']))
-			ace_type = ace['AceType']
-			inherited = bool(ace['AceFlags'] & 0x10)
-			print(f"{identity:<35} {hex(mask):<12} {','.join(rights):<40} {hex(ace_type):<10} {inherited}")
-
-		conn.unbind()
-
-
-
-		"""
 		BRUKERE = [
-			"CN=DRIFT429712,OU=ServiceAccounts,OU=AD,OU=Administrasjon,DC=oslofelles,DC=oslo,DC=kommune,DC=no",
-			"CN=DRIFT429712,OU=DRIFT,OU=Servicekontoer,OU=OK,DC=oslofelles,DC=oslo,DC=kommune,DC=no"
+			"CN=S-BRE-MSCRM-ADMIN,OU=ServiceAccounts,OU=AD,OU=Administrasjon,DC=oslofelles,DC=oslo,DC=kommune,DC=no",
 		]
 
 		def ldap_query_with_sd(ldap_path, ldap_filter, ldap_properties, timeout, sdflags=0x07):
@@ -146,4 +67,4 @@ class Command(BaseCommand):
 				print("Has nTSecurityDescriptor:", bool(has_sd))
 				if has_sd:
 					print("SD bytes:", len(attrs['nTSecurityDescriptor'][0]))
-		"""
+
