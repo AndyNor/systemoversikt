63e8f37bff35e27e30db4326aefeb0dde0f7ba77
André Nordbø <andre.nordbo@gmail.com>
2026-01-15 17:26:29 +0100

    Update manuell_sjekke_ldap.py

--- systemoversikt/management/commands/manuell_sjekke_ldap.py
+++ systemoversikt/management/commands/manuell_sjekke_ldap.py
@@ -4,67 +4,51 @@
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
 import os
+import ldap, os
+from ldap.controls import LDAPControl
+from pyasn1.type.univ import Integer
+from pyasn1.codec.ber import encoder  # <- BER-encode ASN.1 INTEGER
 
 class Command(BaseCommand):
 	def handle(self, **options):
 
 
-		BRUKERE = [
-			"CN=S-BRE-MSCRM-ADMIN,OU=ServiceAccounts,OU=AD,OU=Administrasjon,DC=oslofelles,DC=oslo,DC=kommune,DC=no",
-		]
+		cn = "CN=UKE232914,OU=UKE,OU=Brukere,OU=OK,DC=oslofelles,DC=oslo,DC=kommune,DC=no"
+		server = 'ldaps://ldaps.oslofelles.oslo.kommune.no:636'
+		user = os.environ["KARTOTEKET_LDAPUSER"]
+		password = os.environ["KARTOTEKET_LDAPPASSWORD"]
 
-		def ldap_query_with_sd(ldap_path, ldap_filter, ldap_properties, timeout=10):
-			import ldap, os
-			from ldap.controls import LDAPControl
-			from pyasn1.type.univ import Integer
-			from pyasn1.codec.ber import encoder  # <- BER-encode ASN.1 INTEGER
+		ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+		ldap.set_option(ldap.OPT_PROTOCOL_VERSION, 3)
 
-			server = 'ldaps://ldaps.oslofelles.oslo.kommune.no:636'
-			user = os.environ["KARTOTEKET_LDAPUSER"]
-			password = os.environ["KARTOTEKET_LDAPPASSWORD"]
+		ldap_client = ldap.initialize(server)
+		ldap_client.set_option(ldap.OPT_REFERRALS, 0)
+		ldap_client.bind_s(user, password)
 
-			ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-			ldap.set_option(ldap.OPT_PROTOCOL_VERSION, 3)
+		# SDFlags control: ASN.1 encoded integer
+		sdflags = 0x04  # DACL only
+		control_value = encoder.encode(Integer(sdflags))
+		sd_control = LDAPControl('1.2.840.113556.1.4.801', True, control_value)
 
-			ldap_client = ldap.initialize(server)
-			ldap_client.timeout = timeout
-			ldap_client.set_option(ldap.OPT_REFERRALS, 0)
-			ldap_client.bind_s(user, password)
+		result = ldap_client.search_ext_s(
+			cn,
+			ldap.SCOPE_BASE,
+			'(objectClass=*)',
+			['nTSecurityDescriptor'],
+			serverctrls=[sd_control]
+		)
 
+		ldap_client.unbind_s()
 
-			# SDFlags control: ASN.1 encoded integer
-			sdflags = 0x04  # DACL only
-			control_value = encoder.encode(Integer(sdflags))
-			sd_control = LDAPControl('1.2.840.113556.1.4.801', True, control_value)
-
-			result = ldap_client.search_ext_s(
-				"CN=S-BRE-MSCRM-ADMIN,OU=ServiceAccounts,OU=AD,OU=Administrasjon,DC=oslofelles,DC=oslo,DC=kommune,DC=no",
-				ldap.SCOPE_BASE,
-				'(objectClass=*)',
-				['nTSecurityDescriptor'],
-				serverctrls=[sd_control]
-			)
-
-			ldap_client.unbind_s()
-			return result
+		for dn, attrs in result:
+			if not dn:
+				continue
+			has_sd = 'nTSecurityDescriptor' in attrs and attrs['nTSecurityDescriptor']
+			print("DN:", dn)
+			print("Has nTSecurityDescriptor:", bool(has_sd))
+			if has_sd:
+				print("SD bytes:", len(attrs['nTSecurityDescriptor'][0]))
 
 
-		for bruker in BRUKERE:
 
-			ldap_path = "DC=oslofelles,DC=oslo,DC=kommune,DC=no"
-			ldap_filter = ('(distinguishedName=%s)' % bruker)
-			ldap_properties = ['cn', 'mail', 'givenName', 'displayName', 'sn',
-					'userAccountControl', 'nTSecurityDescriptor']
-
-			result = ldap_query_with_sd(ldap_path, ldap_filter, ldap_properties, timeout=10)
-
-
-			for dn, attrs in result:
-				if not dn:
-					continue
-				has_sd = 'nTSecurityDescriptor' in attrs and attrs['nTSecurityDescriptor']
-				print("DN:", dn)
-				print("Has nTSecurityDescriptor:", bool(has_sd))
-				if has_sd:
-					print("SD bytes:", len(attrs['nTSecurityDescriptor'][0]))
 
