b7ecbf8fe40350fcd4096ddcb1054bb4e62e0a3e
André Nordbø <andre.nordbo@gmail.com>
2026-04-08 19:53:28 +0200

    Update start.sh

--- oslokommune/start.sh
+++ oslokommune/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-source /var/kartoteket/virtualenv/py39/bin/activate
+source /var/kartoteket/virtualenv/py311/bin/activate
 #export LD_LIBRARY_PATH="/usr/local/lib/"
 cd /var/kartoteket/source/systemoversikt/
