c635541186b51521b5ccf4d0335114a81566d8d9
André Nordbø <andre.nordbo@gmail.com>
2026-05-15 11:35:03 +0200

    Update restart.sh

--- oslokommune/restart.sh
+++ oslokommune/restart.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+echo "Reloading deamons.."
+sudo systemctl daemon-reload
 echo "Restarting gunicorn.."
 sudo systemctl restart gunicorn-mysite.service
 echo "Restarting webserver.."
 sudo systemctl restart httpd.service
+echo "Restarting database.."
+sudo systemctl restart postgrsql.service
 echo "Complete"
